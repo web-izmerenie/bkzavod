3a33ac0588dedcb2fa07a16f0eb937134f9db676
Andrey Chechkin <andrey.chechkin@web-izmerenie.ru>
2015-05-14 13:50:47 +0300

    Add catalog sections

---
+++ bitrix/templates/.default/components/bitrix/catalog.section.list/main_catalog_sections/template.php
@@ -0,0 +1,30 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+$this->setFrameMode(true);?>
+<?if(!empty($arResult['SECTIONS'])){?>
+	<section class="main-catalog">
+		<p>Выберите интересующий Вас 
+		цвет кирпича:</p>
+		<ul>
+			<?foreach($arResult['SECTIONS'] as $arItem){?>
+			<li style="color:<?=$arItem['UF_COLOR']?>;">– <a href="<?=$arItem['SECTION_PAGE_URL']?>">
+				<?=$arItem['UF_COLOR_NAME']?>
+				<?if($arItem['UF_TOP'] == 1){?>
+					<img style="fill:<?=$arItem['UF_COLOR']?>;" class="svg" src="/bitrix/templates/main/images/star138.svg">
+				<?}?>
+				</a>
+			</li>
+			<?}?>
+		</ul>
+	</section>
+<?}?>
--- produktsiya/index.php
+++ produktsiya/index.php
@@ -1,4 +1,35 @@
-<?
+<?define('CATALOG', 'Y');
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Каталог Продукции");
-?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+?><?$APPLICATION->IncludeComponent(
+	"bitrix:catalog.section.list", 
+	"main_catalog_sections", 
+	array(
+		"IBLOCK_TYPE" => "catalog",
+		"IBLOCK_ID" => "2",
+		"SECTION_ID" => "",
+		"SECTION_CODE" => $_REQUEST["SECTION_CODE"],
+		"COUNT_ELEMENTS" => "Y",
+		"TOP_DEPTH" => "2",
+		"SECTION_FIELDS" => array(
+			0 => "",
+			1 => "undefined",
+			2 => "",
+		),
+		"SECTION_USER_FIELDS" => array(
+			0 => "UF_COLOR",
+			1 => "UF_TOP",
+			2 => "UF_COLOR_NAME",
+			3 => "undefined",
+			4 => "",
+		),
+		"VIEW_MODE" => "LIST",
+		"SHOW_PARENT_NAME" => "Y",
+		"SECTION_URL" => "",
+		"CACHE_TYPE" => "A",
+		"CACHE_TIME" => "36000000",
+		"CACHE_GROUPS" => "Y",
+		"ADD_SECTIONS_CHAIN" => "Y"
+	),
+	false
+);?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
